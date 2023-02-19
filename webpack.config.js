640c0bc6d684ff5bb0f2964cc56cab1ed787bd31
uljdcwy_TOu1o <uljdcwykhgm@outlook.com>
2023-02-19 17:33:23 +0800

    2023-2-19 编译环境优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
-module.exports = {
-    entry: './pages/testj.js',
-    mode: "none",
-    output: {
-        filename: "test.js"
+const path = require("path");
+
+module.exports = (env) => {
+    console.log(env,"“")
+    return {
+        entry: './pages/testj.js',
+        target: 'web',
+        mode: env?.production ? 'production' : 'development',
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+            },
+        },
+        resolve: {
+            fallback: {
+                'path': false
+            },
+        },
+        output: {
+            filename: './js/[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        }
     }
 }
