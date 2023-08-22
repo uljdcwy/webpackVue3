d8ea1db3884d559fd9a43664f8342cef8340a793
zxy <uljdcwykhgm@outlook.com>
2023-08-22 16:13:13 +0800

    2023-08-22 BUG调整

---
+++ Services/config.ts
@@ -0,0 +1,4 @@
+export default {
+    sercert: "zxy",// 密钥编码
+
+}
--- Services/middle/desensitization.ts
+++ Services/middle/desensitization.ts
@@ -1,27 +1,38 @@
 import CryptoJS from "crypto-js"
+import config from "../config";
 
 export enum EncryptionType {
+    // 加密手机号枚举
     Phone,
-    Identification
+    // 加密身分证号枚举
+    Identification,
+    // 身份证名称
+    Name
 }
-
-const sercert = "123456"
-
+// 获取全局配置中的密钥
+const sercert = config.sercert;
+// 对字符进行加密，并返回加密后的内容
 export const encryption = (encrypted: any, type: EncryptionType = EncryptionType.Phone, level: number = 3): any => {
-    let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(encrypted), sercert).toString();
-
     encrypted = (typeof encrypted != 'string') ? JSON.stringify(encrypted) : encrypted;
-
+    let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(encrypted), sercert).toString();
+    
+    // 验证类型为手机号进行加密
     if (type == EncryptionType.Phone) {
         encrypted = encrypted.replace(/(.{3})(.{6})/g, (expStr, $1, $2) => {
             return $1 + "******";
         })
+        // 验证类型为身份证号进行加密
     } else if (type == EncryptionType.Identification) {
         encrypted = encrypted.replace(/(.{6})(.{8})/g, (expStr, $1, $2) => {
             return $1 + "********";
         })
+        // 验证类型为身份证名称进行加密
+    } else if (type == EncryptionType.Name) {
+        encrypted = encrypted.replace(/(.{1})(.+)/g, (expStr, $1, $2) => {
+            return $1 + $2.replace(/./g,"*");
+        })
     }
-
+    // 默认安全等级
     if (level != 3) {
         encrypted
     };
@@ -32,7 +43,7 @@ export const encryption = (encrypted: any, type: EncryptionType = EncryptionType
         ciphertext
     }
 }
-
+// 解密加密后的编码
 export const decrypt = (ciphertext: string) => {
     let bytes = CryptoJS.AES.decrypt(ciphertext, sercert);
      return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
