335c51768e59ea4de35c9b795a79a1191f4c5ba8
zhengxiya <uljdcwykhgm@Outlook.com>
2023-11-26 18:54:45 +0800

    2023-11-26 更新部分组件

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,7 @@
     <div class="banner">
         <swiper-index :data="swiperData"></swiper-index>
     </div>
-    <main-context></main-context>
+    <main-context :editData="editData" bgUrl="http://www.sc-qianye.com/templates/main/images/section5_bg.jpg"></main-context>
 </template>
 <script setup>
 import { onMounted, ref } from "vue";
@@ -22,6 +22,15 @@ const swiperData = [{
     src: "http://www.sc-qianye.com/upload/202309/21/202309211009276131.jpg",
     alt: "搜索引擎抓取的内容"
 }]
+
+const editData  = {
+  vice: "about",
+  title: "关于千页",
+  describe: "四川千页科技股份有限公司，是一家集研发、制造、销售于一体的新型技术企业，涉足储能预警、储能消防系统、电池舱自动灭火装置等产品，专注为电化学储能、电力电网、文物古建筑、轨道交通、石油石化、应急消防等各行业提供个性化的消防技术解决方案。",
+  detailsUrl: "javascript:;",
+  imgUrl: "http://www.sc-qianye.com/templates/main/images/section5_img.jpg",
+  imgAlt: ""
+}
 
 // @ts-ignore   获取SSR传过来的初始化的数据
 let ssrData = ref(props.initData || global._INIT_DATA_);
--- src/components/mainContext/index.vue
+++ src/components/mainContext/index.vue
@@ -1,40 +1,166 @@
 <template>
     <div class="context-block">
         <div class="main-context">
-            <div class="main-left">
-                <div class=""></div>
+            <div class="context-text">
+                <h3 class="vice-title">{{ editData.vice }}</h3>
+                <h2 class="main-title">{{ editData.title }}</h2>
+                <span class="title-line"></span>
+                <div class="title-describe">
+                    {{ editData.describe }}
+                </div>
+                <a :href="editData.detailsUrl" class="look-details">了解详情</a>
             </div>
-            <div class="main-right">
-                <img src="http://www.sc-qianye.com/templates/main/images/section5_img.jpg" alt="">
+            <div class="context-image">
+                <img :src="editData.imgUrl" :alt="editData.imgAlt">
             </div>
         </div>
     </div>
 </template>
 <script setup>
-    import { ref } from "vue";
-    const props = defineProps(['bgUrl']);
+import { ref } from "vue";
+const props = defineProps(["bgUrl", "editData"]);
 
-    /**
-     * @type { String }
-     */
-    const bgUrl = props.bgUrl || "";
+/**
+ * @type { String }
+ */
+const bgUrl = props.bgUrl;
 
-    const bg = `url(${bgUrl})`;
+/**
+ * @type { { vice: string, title: string, describe: string, detailsUrl: string, imgUrl: string, imgAlt: string } }
+ */
+const editData = props.editData;
 
-    
+const bg = `url(${bgUrl})`;
+
+
 </script>
 
 <style lang="scss" scoped="scoped">
 @import "@/scss/class.scss";
 @import "@/scss/theme.scss";
 
-.context-block{
+.context-block {
     background-image: v-bind(bg);
+    @include margin(20, 0);
+    @include padding(25, 0);
 }
 
+
 .main-context {
+    display: flex;
+    justify-content: space-between;
+    background-color: $defaultBGColor;
+
+    .context-text {
+        flex: 1;
+
+        .vice-title {
+            @include font(30);
+            color: $lightColor;
+        }
+
+        .title-describe {
+            @include font(14);
+            @include padding(10, 0)
+        }
+
+        .title-line {
+            display: block;
+            @include width(40);
+            @include height(4);
+            background-color: $redColor;
+        }
+
+        .main-title {
+            @include padding(10, 0, 20);
+            @include font(42);
+        }
+
+        .look-details {
+            @include font(14);
+            color: $linkColor;
+            text-decoration-line: none;
+
+            &:hover {
+                color: $hoverColor;
+            }
+        }
+
+    }
+
+    .context-image {
+        width: 50%;
+
+        img {
+            max-width: 100%;
+        }
+    }
+
     @include width($mainWidth);
-    @include margin(0, -1);
+    @include margin(20, -1);
     @include padding(20)
 }
-</style>
+
+@media screen and (max-width: 640px) {
+    .context-block {
+        background-image: v-bind(bg);
+        @include margin(10, 0);
+        @include padding(10, 0);
+    }
+
+
+    .main-context {
+        background-color: $defaultBGColor;
+        width: 100%;
+        display: block;
+        @include margin(10);
+        .context-text {
+            @include padding(15);
+            @include margin(10);
+            .vice-title {
+                @include font(30);
+                color: $lightColor;
+            }
+
+            .title-describe {
+                @include font(14);
+                @include padding(10, 0)
+            }
+
+            .title-line {
+                display: block;
+                @include width(40);
+                @include height(4);
+                background-color: $redColor;
+            }
+
+            .main-title {
+                @include padding(10, 0, 20);
+                @include font(42);
+            }
+
+            .look-details {
+                @include font(14);
+                color: $linkColor;
+                text-decoration-line: none;
+
+                &:hover {
+                    color: $hoverColor;
+                }
+            }
+
+        }
+
+        .context-image {
+            width: 100%;
+            box-sizing: border-box;
+            @include padding(20);
+
+            img {
+                max-width: 100%;
+            }
+        }
+        width: auto;
+        @include padding(0);
+    }
+}</style>
