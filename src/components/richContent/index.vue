0dfa6d5150f011d037acd77187efe5483f646c39
zhengxiya <uljdcwykhgm@Outlook.com>
2024-01-20 17:33:41 +0800

    2023-01-20 更新内容

--- src/components/richContent/index.vue
+++ src/components/richContent/index.vue
@@ -1,176 +1,68 @@
 <template>
-    <div id="editor" style="height: 300px;background-color: #f00;"></div>
+    <div style="border: 1px solid #ccc">
+        <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef" :defaultConfig="toolbarConfig" :mode="mode" />
+        <Editor :maxLength="120" style="height: 500px; overflow-y: hidden;" id="editorEl" ref="editorElRef"
+            v-model="valueHtml" :defaultConfig="editorConfig" :mode="mode" @onCreated="handleCreated" />
+        {{ editorConfig.placeholder }}
+    </div>
 </template>
 
 <script setup>
-import 'reflect-metadata'
-import { BrowserModule } from '@textbus/platform-browser'
-import {
-    Commander,
-    ContentType,
-    createVNode,
-    defineComponent,
-    onBreak, onContentInsert,
-    Selection,
-    Slot,
-    Textbus,
-    useContext,
-    useSelf,
-} from '@textbus/core'
-import { createApp, defineComponent as defineVue, onMounted } from 'vue'
-import { Adapter, ViewComponentProps } from '@textbus/adapter-vue';
 
+import { onBeforeUnmount, ref, shallowRef, onMounted, reactive, effect } from 'vue'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+
+// 编辑器实例，必须用 shallowRef
+const editorRef = shallowRef()
+
+// 内容 HTML
+const valueHtml = ref('');
+
+const editorElRef = ref();
+
+const mode = ref("default");
+// 模拟 ajax 异步获取内容
 onMounted(() => {
+    setTimeout(() => {
+        // editorRef.value.dangerouslyInsertHtml(`<i class="fa fa-check" style="color: blue">123</i>`)
+        // editorConfig.value = { placeholder: "123456789" };
+        // console.log(123456789, editorRef.value)
+    }, 3000);
+});
 
-    // 创建视图 Vue 根组件
-    const rootComponentView = defineVue({
-        props: ['component', 'rootRef'],
-        setup(props) {
-            return () => {
-                const slot = props.component.slots.first
-                return (
-                    adapter.slotRender(slot, children => {
-                        return createVNode('div', {
-                            'textbus-document': 'true',
-                            ref: props.rootRef
-                        }, children)
-                    })
-                )
-            }
-        }
-    })
+const toolbarConfig = {};
 
-    // 创建视图 Vue 段落组件
-    const paragraphView = defineVue({
-        props: ['component', 'rootRef'],
-        setup(props) {
-            return () => {
-                const slot = props.component.slots.first
-                return (
-                    adapter.slotRender(slot, children => {
-                        return createVNode('p', { ref: props.rootRef }, children)
-                    })
-                )
-            }
-        }
-    })
-
-    // 创建 Textbus 根组件
-    const rootComponent = defineComponent({
-        name: 'RootComponent',
-        type: ContentType.BlockComponent,
-        validate(initData) {
-            return {
-                slots: [
-                    initData?.slots?.[0] || new Slot([
-                        ContentType.Text,
-                        ContentType.InlineComponent,
-                        ContentType.BlockComponent
-                    ])
-                ]
-            }
+const editorConfig = ref({
+    placeholder: '请输入内容...' ,
+    MENU_CONF: {
+        bgColor: {
+            colors: ['#f00', '#f00', '#f00']
         },
+        lineHeightList: ['1', '1.5', '2', '2.5']
+    }
+});
 
-        setup() {
-            const selection = useContext(Selection)
-            const textbus = useContext()
-            // 监听内容插入事件
-
-            onContentInsert(ev => {
-                // 当插入的内容是一个字符串或行内组件时，我们将创建新的段落
-                if (typeof ev.data.content === 'string' || ev.data.content.type !== ContentType.BlockComponent) {
-                    // 创建新的插槽，并把内容插入在新插槽内
-                    const slot = new Slot([
-                        ContentType.Text,
-                        ContentType.InlineComponent
-                    ])
-                    slot.insert(ev.data.content)
-
-                    // 创建新的段落组件，并把插槽传给段落组件
-                    const p = paragraphComponent.createInstance(textbus, {
-                        slots: [slot]
-                    })
-                    // 在 rootComponent 的插槽内插入新段落
-                    ev.target.insert(p)
-                    // 设置光标为段落组件插槽的索引位置
-                    selection.setPosition(slot, slot.index)
-                    // 阻止默认的插入事件
-                    ev.preventDefault()
-                }
-            })
-        }
-    })
-
-    // 创建 Textbus 段落组件
-    const paragraphComponent = defineComponent({
-        name: 'ParagraphComponent',
-        type: ContentType.BlockComponent,
-        validate(initData) {
-            return {
-                slots: [
-                    initData?.slots?.[0] || new Slot([
-                        ContentType.Text,
-                        ContentType.InlineComponent
-                    ])
-                ]
-            }
-        },
-        setup() {
-            const context = useContext()
-            const commander = useContext(Commander)
-            const selection = useContext(Selection)
-            const self = useSelf()
-
-            onBreak(ev => {
-                ev.preventDefault()
-                const nextContent = ev.target.cut(ev.data.index)
-                const p = paragraphComponent.createInstance(context, {
-                    slots: [nextContent]
-                })
-                commander.insertAfter(p, self)
-                selection.selectFirstPosition(p)
-            })
-        }
-    })
-
-    // 实例化 Vue 适配器
-    const adapter = new Adapter({
-        // 添加渲染组件映射关系
-        [rootComponent.name]: rootComponentView,
-        [paragraphComponent.name]: paragraphView
-    }, (host, root) => {
-        // 使用 Vue 渲染 Textbus 视图
-        const app = createApp(root).provide('textbus', textbus)
-        app.mount(host)
-        return () => {
-            app.unmount()
-        }
-    })
-    // 实例化浏览器模块
-    const browserModule = new BrowserModule({
-        adapter, // 添加 Vue 适配器
-        renderTo() {
-            return document.getElementById('editor')
-        }
-    })
-    // 实例化 Textbus
-    const textbus = new Textbus({
-        imports: [
-            browserModule
-        ],
-        components: [
-            rootComponent,
-            paragraphComponent
-        ]
-    })
-
-    // 创建根组件实例
-    const rootModel = rootComponent.createInstance(textbus)
-
-    // 使用 Textbus 启动渲染
-    textbus.render(rootModel)
+effect(() => {
+    let editorDom = document.getElementById("editorEl");
+    let placeholderDep = editorConfig.value.placeholder;
+    if (editorDom) {
+        let placeHolderDom = editorDom.getElementsByClassName("w-e-text-placeholder")[0];
+        placeHolderDom.innerText = placeholderDep;
+    }
 })
 
+// 组件销毁时，也及时销毁编辑器
+onBeforeUnmount(() => {
+    const editor = editorRef.value
+    if (editor == null) return
+    editor.destroy()
+})
+
+const handleCreated = (editor) => {
+    editorRef.value = editor // 记录 editor 实例，重要！
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@wangeditor/editor/dist/css/style.css' // 引入 css
+</style>
