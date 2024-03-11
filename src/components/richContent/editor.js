0ffe795d16ee69211354b66a18eb086cb1d5b996
zhengxiya <uljdcwykhgm@Outlook.com>
2024-03-11 15:59:34 +0800

    2024-03-11 更新部分BUG

--- src/components/richContent/editor.js
+++ src/components/richContent/editor.js
@@ -3,7 +3,7 @@ export const getDomJson = (rootDom = document.body) => {
     return breadthCycle(rootDom, null);
 };
 // 字符串化JSON
-export const stringJson = (json) => {
+export const stringJson = (/** @type {{ children: any; attrs: { [x: string]: string; }; tag: any; }} */ json) => {
     let str = "";
     let childrens = json.children;
     if (Array.isArray(childrens)) {
@@ -24,7 +24,7 @@ export const stringJson = (json) => {
     }
 };
 // 更新
-export const patch = (oldVdom, newVdom) => {
+export const patch = (/** @type {{ children: any; el: { nodeType: string; parent: { replaceChild: (arg0: any, arg1: any) => void; }; }; }} */ oldVdom, /** @type {{ children: any; el: any; }} */ newVdom) => {
 
 };
 // 节点类型
@@ -33,7 +33,7 @@ const nodeTypes = {
     "3": "text",
 };
 // 获取惟一键
-const getuuid = (len) => {
+const getuuid = (/** @type {number} */ len) => {
     const uuids = "qwertyuiopasdfghjklzxcvbnm123456789QWERTYUIOPASDFGHJKLZXCVBNM";
     let key = "";
     let lens = uuids.length;
@@ -43,27 +43,50 @@ const getuuid = (len) => {
     return key;
 };
 // 获取属性
+/**
+ * 
+ * @param {*} el 
+ * @param {*} nodeType 
+ * @returns 
+ */
 const getAttrs = (el, nodeType) => {
-    const attrs = el.attributes;
-    let oAttr;
-    if (nodeType == 'tag') {
-        oAttr = {};
-        let attrLen = attrs.length;
-        for (let i = 0; i < attrLen; i++) {
-            oAttr[attrs[i].nodeName] = attrs[i].nodeValue;
-        };
+    /** @type {any} */
+    let oAttr = {};
+    if(typeof el == "string") {
+        el = el.trim();
+        el = el.replace(/([a-zA-z ]+)=(("|')[^"']+("|'))/g, (/** @type {any} */ a,/** @type {string} */ b,/** @type {string} */ c) => {
+            oAttr[b.trim()] = c.trim();
+            return "";
+        });
+
+        let elAttr = el.split(" ");
+        elAttr.map((/** @type {string} */ key) => {
+            oAttr[key.trim()] = "";
+        })
+
+
+    } else {
+        const attrs = el.attributes;
+        if (nodeType == 'tag') {
+            let attrLen = attrs.length;
+            for (let i = 0; i < attrLen; i++) {
+                oAttr[attrs[i].nodeName] = attrs[i].nodeValue;
+            };
+        }
     }
     return oAttr;
 };
 // 深度循环
-const deepLoop = (domEls, root, parent) => {
+const deepLoop = (/** @type {string | any[] | NodeListOf<ChildNode>} */ domEls, /** @type {any[] | undefined} */ root, /** @type {{ nodeType: any; tag: string | undefined; el: HTMLElement; key: string; children: never[]; parent: undefined; attrs: any; selected: never[]; } | undefined} */ parent) => {
     let elsLen = domEls.length;
     for (let i = 0; i < elsLen; i++) {
+        // @ts-ignore
         breadthCycle(domEls[i], root, parent);
     }
 };
 // 广度循环
-const breadthCycle = (domEl, root = [], parent) => {
+// @ts-ignore
+const breadthCycle = (/** @type {HTMLElement} */ domEl, root = [], /** @type {undefined} */ parent) => {
     const childrens = domEl.childNodes;
     const nodeType = nodeTypes[domEl.nodeType];
     let elVDom = {
@@ -86,13 +109,13 @@ const breadthCycle = (domEl, root = [], parent) => {
     return root
 };
 // 小写字符串
-const toLocalLow = (str, nodeType) => {
+const toLocalLow = (/** @type {string} */ str, /** @type {string} */ nodeType) => {
     if (nodeType == "tag") {
         return str.toLocaleLowerCase()
     }
 };
 // 获取文本内容
-const getTextContent = (domEl, nodeType) => {
+const getTextContent = (/** @type {{ innerText: string; nodeValue: string; }} */ domEl, /** @type {any} */ nodeType) => {
     let child = "";
     switch (nodeType) {
         case 'tag':
@@ -105,7 +128,7 @@ const getTextContent = (domEl, nodeType) => {
     return child
 };
 // 替换特殊内容
-const replaceSpecify = (elVDom) => {
+const replaceSpecify = (/** @type {{ nodeType?: any; tag?: any; el?: any; key?: string; children?: any; parent: any; attrs?: any; selected?: never[]; }} */ elVDom) => {
     if (elVDom.parent && elVDom.parent.tag == "div") {
         replacePToDIVElement(elVDom);
     } else if (hasParagraph(elVDom)) {
@@ -117,7 +140,8 @@ const replaceSpecify = (elVDom) => {
     }
 };
 // 当前当前文本是否有段落
-const hasParagraph = (vDom) => {
+// @ts-ignore
+const hasParagraph = (/** @type {{ parent: { tag: string; }; }} */ vDom) => {
     if (!vDom.parent) {
         return false;
     } else if (vDom.parent.tag == 'p') {
@@ -127,7 +151,7 @@ const hasParagraph = (vDom) => {
     }
 };
 // 将DIV标签替换成P
-const replacePToDIVElement = (elVDom) => {
+const replacePToDIVElement = (/** @type {{ parent?: any; el?: any; attrs?: any; tag?: any; children?: any; }} */ elVDom) => {
     let divVdom = elVDom.parent;
     if (!divVdom.parent) {
         if(divVdom.tag != 'span'){
@@ -155,23 +179,26 @@ const replacePToDIVElement = (elVDom) => {
     moveCursorToEnd(p);
 };
 
-const patchJson = (oldJson, newJson) => {
+const patchJson = (/** @type {{ tag?: any; parent?: { tag: string; } | { el: Node; }; el?: any; children?: any; }} */ oldJson, /** @type {any[]} */ newJson) => {
     oldJson.tag = newJson.tag;
     oldJson.el = newJson.el;
     oldJson.children = newJson.children;
 }
 
 // 添加段落标签
-const addParagraph = (elVDom) => {
+const addParagraph = (/** @type {{ parent: { tag: string; }; el: { cloneNode: (arg0: boolean) => any; }; }} */ elVDom) => {
     if (!elVDom.parent) return;
     const p = document.createElement("p");
     let getDomCopy;
     if (elVDom.parent.tag == "span") {
         const parentVDOM = elVDom.parent;
+        // @ts-ignore
         if(!parentVDOM.parent) return ;
+        // @ts-ignore
         getDomCopy = parentVDOM.el.cloneNode(true);
         p.appendChild(getDomCopy);
         let appendJson = getDomJson(p);
+        // @ts-ignore
         parentVDOM.parent.el.replaceChild(p, parentVDOM.el);
         patchJson(parentVDOM, appendJson);
     } else {
@@ -181,24 +208,26 @@ const addParagraph = (elVDom) => {
         p.appendChild(span);
         span.appendChild(getDomCopy);
         let appendJson = getDomJson(p);
+        // @ts-ignore
         parentVDOM.el.replaceChild(p, elVDom.el);
         patchJson(elVDom, appendJson);
     };
     moveCursorToEnd(p);
 };
 // 将没有span包住的文本元素替换成SPAN包住的文本元素
-const replaceSpanToTextElement = (elVDom) => {
+const replaceSpanToTextElement = (/** @type {{ el: { cloneNode: (arg0: boolean) => any; }; parent: { el: Node; }; }} */ elVDom) => {
     const span = document.createElement("span");
     const cloneText = elVDom.el.cloneNode(true);
     span.appendChild(cloneText);
     console.log(elVDom,"elVDom")
+    // @ts-ignore
     elVDom.parent.el.replaceChild(span, elVDom.el);
     const replaceJson = getDomJson(span);
     patchJson(elVDom, replaceJson);
     moveCursorToEnd(elVDom.parent.el);
 };
 // 更新属性
-const patchAttrs = (oldVdom, newVdom) => {
+const patchAttrs = (/** @type {any[]} */ oldVdom, /** @type {{ attrs: any; }} */ newVdom) => {
     let oldAttr = oldVdom.attrs;
     let newAttr = newVdom.attrs;
     let resetUndefined;
@@ -224,7 +253,7 @@ const patchAttrs = (oldVdom, newVdom) => {
 };
 
 // 更新文本
-const patchText = (oldVdom, newVdom) => {
+const patchText = (/** @type {{ children: any; el: { nodeType: string; parent: { replaceChild: (arg0: any, arg1: any) => void; }; }; }} */ oldVdom, /** @type {{ children: any; el: any; }} */ newVdom) => {
     if (typeof oldVdom.children == 'string' && !Array.isArray(newVdom.children) && newVdom.children != oldVdom.children || typeof newVdom.children == 'string') {
         oldVdom.children = newVdom.children;
         let updateElem = oldVdom.el.nodeType == 'text' ? oldVdom.el.parent : oldVdom.el;
@@ -236,7 +265,7 @@ const patchText = (oldVdom, newVdom) => {
     }
 };
 // 到动光标到当前元素的最后方
-const moveCursorToEnd = (element) => {
+const moveCursorToEnd = (/** @type {Node} */ element) => {
     var range = document.createRange();
     var selection = window.getSelection();
 
@@ -262,7 +291,7 @@ const selectStart = () => {
     return selectStatus;
 };
 // 选择移动时
-const selectMove = (e) => {
+const selectMove = (/** @type {any} */ e) => {
     if(!selectStatus) return ;
     // 标记选择 文本 或内容放到 selected  
     console.log(e, "查看选择的元素")
@@ -276,9 +305,138 @@ const selectEnd = () => {
 };
 
 // 将字符串化的 json 格式化为 AST;
-const jsonStrToAst = () => {
+/**
+ * 
+ * @param {string} str 
+ */
+export const jsonStrToAst = (str) => {
+    /** @type {any} */
+    let runObj = {};
+    /** @type {any} */
+    let activeObject;
 
+    runObj.originSource = str;
+    runObj.tagReg = /^<\/?([^>]+)>/i;
+    runObj.textReg = /([^<]+)</i;
+    runObj.offsetLeft = 0;
+    runObj.tagStack = [];
+    runObj.ast = null;
+    runObj.__proto__.mountToEl = mountToEl;
+    let i = 0;
+    
+    while(str) {
+        /** @type {any} */
+        let startMatch = str.match(runObj.tagReg);
+        /** @type {string} */
+        let tagMark = startMatch && startMatch[0];
+        let tagName = startMatch && startMatch[1];
+        let isEndTag = str.startsWith(tagMark) && str.startsWith("</");
+        let subStrLen = tagMark && tagMark.length || 0;
+        let strlen = str.length;
+        /** @type {any} */
+        let tagProps = tagName && tagName.replace(/\S+/, (/** @type {any} */ a) => {
+            tagName = a;
+            return "";
+        });
+        let nodeType;
+
+
+
+        /** @type {any} */
+        let initObject = {
+            nodeType: 1,
+            tag: tagName,
+            position: [runObj.offsetLeft],
+            key: getuuid(24),
+            children: [],
+            parent: null,
+            attrs: null,
+            selected: []
+        };
+
+        if(isEndTag) {
+            let popStack = activeObject;
+            if(popStack.tag == tagName){
+                activeObject.position.push(runObj.offsetLeft + subStrLen)
+                activeObject = activeObject.parent
+            }else{
+                subStrLen = 0;
+                runObj.tagStack.push(popStack);
+                console.warn("标签对错误 标签名称：" + popStack.tag + "; 标签位置：" + runObj.offsetLeft);
+            }
+        } else if(str.startsWith(tagMark)){
+                
+            initObject.tag = tagName;
+            initObject.attrs = getAttrs(tagProps, nodeType);
+            
+            if(activeObject){
+                let pushObject = Array.isArray(activeObject) ? activeObject : activeObject.children;
+                pushObject.push(initObject);
+                initObject.parent = activeObject;
+                activeObject = activeObject.children[activeObject.children.length - 1];
+            }else{
+                initObject.parent = null;
+                runObj.tagStack.push(initObject);
+                activeObject = runObj.tagStack[0];
+            }
+            let currentElement = document.createElement(tagName);
+            initObject.parent && initObject.parent.el.appendChild(currentElement);
+            initObject.el = currentElement;
+        }else{
+            let textMatch = str.match(runObj.textReg);
+            /** @type {any} */
+            let textContent = textMatch && textMatch[1];
+            subStrLen = textContent.length || 0;
+            initObject.nodeType = 3;
+            initObject.parent = activeObject;
+            initObject.tag = null;
+            initObject.children = textContent;
+            initObject.position = [runObj.offsetLeft, runObj.offsetLeft + subStrLen];
+            activeObject.children = initObject;
+            let currentTextNode = document.createTextNode(textContent);
+            initObject.parent && initObject.parent.el.appendChild(currentTextNode);
+            initObject.el = currentTextNode
+        };
+        str = str.substring(subStrLen, strlen);
+        runObj.offsetLeft += subStrLen;
+    }
+    return runObj;
 }
+
+const mountToEl = /**
+ * @this {any}
+ */
+ function (/** @type {string} */ idName) {
+    let idElement = document.getElementById(idName);
+    this.tagStack[0].children.map((/** @type {{ el: any; }} */ el) => {
+        // @ts-ignore
+        idElement.appendChild(el.el);
+    })
+}
+
+// 判断是自闭合标签
+/**
+ * 
+ * @param {string|null} tag 
+ * @returns 
+ */
+const isSelfClosing = (tag) => {
+    let status = false;
+    switch(tag){
+        case "br":
+        case "img":
+        case "hr":
+        case "input":
+            status = true;
+            break;
+        default:
+            status = false;
+            break;
+    };
+    return status;
+}
+
+// 更新 字符串 AST 到 DOM
 
 // 修复替换元素出现的 BUG 调整中
 
--- src/components/richContent/index.vue
+++ src/components/richContent/index.vue
@@ -1,25 +1,22 @@
 <template>
     <div id="toolbar"></div>
-    <div id="editMain" contenteditable="true" @keydown="clearKeyUpKey" @keyup="getEditorJson" style="height: 120px;background-color: #f00;">
+    <div id="editMain" contenteditable = "true" @keyup="getEditorJson" style="height: 120px;background-color: #f00;">
     </div>
 </template>
 
 <script setup>
 import { effect, onMounted, onUnmounted } from 'vue';
-import { getDomJson, stringJson } from "./editor.js";
+import { getDomJson, stringJson, jsonStrToAst } from "./editor.js";
 /** @type { any } */
 let editMain;
 let agentStart = false;
-const getEditorJson = (e) => {
-  window.upKey = e.keyCode;
+const getEditorJson = (/** @type {any} */ e) => {
   if(agentStart) return;
   setTimeout(() => {
-    getDomJson(editMain)
-  })
-};
+    let ast = getDomJson(editMain);
 
-const clearKeyUpKey = () => {
-  window.upKey = null;
+    console.log(jsonStrToAst(`<div data-v-7334d060 id=\"editMain\" contenteditable=\"true\" style=\"height: 120px; background-color: rgb(255, 0, 0);\" ><p ><span >zhen</span></p><p ><span >gxi2</span></p><p ><span >ya</span></p></div>`).mountToEl("editMain"), "ast 文本内容", ast)
+  })
 };
 
 const startAgentFn = () => {
